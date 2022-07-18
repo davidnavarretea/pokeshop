1478f37dbae39c24021d5de1a69c1835b3240a24
David <davidnavarretea1996@gmail.com>
2022-07-18 04:55:37 +0200

    nosotros finished

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -15,13 +15,13 @@ const Nosotros = () => {
               <Image className={styles.img} layout="fixed" width={200} height={200} src='/img/pokemon1.webp' alt="pokemon nft"/>;
               <p className={styles.colortext}>NFTs de Pokemon, invierte directamente en OpenSea, sin intermediarios</p>
               <Image className={styles.img} layout="fixed" width={200} height={200} src='/img/pokemon2.webp' alt="nft pokemon"/>;
-              <p className={styles.colortext}>Si en 1 año, no le sacas rendimiento al NFT, te lo recompramos por el valor adquirido, porque creemos en nuestros Pokemon</p>
+              <p className={styles.colortext}>1 año de garantia de inversión, si el NFT no sube de precio, reembolso completo</p>
             </div>
             <div className={styles.texton}>
             <Image className={styles.img} layout="fixed" width={200} height={200} src='/img/pokemon4.webp' alt="nfts pokemon"/>;
-            <p className={styles.colortext}>Top 9% de los diseñadores freelancers, este marketplace de NFTs de Pokemon es para ti</p>
+            <p className={styles.colortext}>Top 9% de los diseñadores freelancers: este marketplace de NFTs de Pokemon es para ti</p>
             <Image className={styles.img} layout="fixed" width={200} height={200} src='/img/pokemon3.webp' alt="pokemon nfts"/>;
-            <p className={styles.colortext}>Si eres coleccionista, solo se postean los mejores NFTs, de los mejores diseñadores de NFTs de Pokemon</p>
+            <p className={styles.colortext}>¿Eres coleccionista?, solo posteamos los mejores NFTs, de los mejores diseñadores de NFTs Pokemon</p>
             </div>
           </div>
         </main>
