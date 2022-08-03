a6d4184e88c8f1d9bbc56241db6d1fb9e027d98f
David <davidnavarretea1996@gmail.com>
2022-08-03 04:35:31 +0200

    nftadd

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,12 +1,16 @@
+import Image from "next/image"
 import Layout from "../components/Layout"
+import styles from '../styles/Tienda.module.css'
 const Tienda = () => {
   return (
     <Layout
         page='Tienda'
     >
-        <h2 className="heading">
-          Tienda
-        </h2>
+      <main className={`contenedor ${styles.contenedor2}`}>
+        <a href='https://opensea.io/assets/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/29963882739591922624539360890442999687194626454638703259683535423299133636609/' target='_blank' rel="noopener noreferrer">
+          <Image className={styles.img} layout='responsive' width={380} height={529} src='/img/marco.webp' alt='pokemon nft'/>
+        </a>
+      </main>
     </Layout>
   )
 }
